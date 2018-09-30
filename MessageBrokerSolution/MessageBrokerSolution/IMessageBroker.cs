dc23eb30dfe9873cb51217129e08b969d8d7370a
Chandresh Makwana <Chandresh.Makwana@harman.com>
2018-09-30 23:09:57 +0530

    Added the first check-in.

---
+++ MessageBrokerSolution/MessageBrokerSolution/IMessageBroker.cs
@@ -0,0 +1,11 @@
+﻿namespace MessageBroker
+{
+    using System;
+    public interface IMessageBroker : IDisposable
+    {
+        void Publish<T>(object source, T message);
+        void Subscribe<T>(Action<MessagePayload<T>> subscription);
+        void Unsubscribe<T>(Action<MessagePayload<T>> subscription);
+        bool IsPersistent { get; }
+    }
+}
