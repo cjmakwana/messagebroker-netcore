2de1137b0809463a4cf436d95f7cdd4558ae13f6
Chandresh Makwana <Chandresh.Makwana@harman.com>
2018-09-30 23:41:08 +0530

    69dcade0822c6025316c035fa16f5903b9a3b50c

--- MessageBrokerSolution/MessageBroker/MessageBrokerImpl.cs
+++ MessageBrokerSolution/MessageBroker/MessageBrokerImpl.cs
@@ -1,4 +1,4 @@
-﻿namespace MessageBroker
+namespace MessageBroker
 {
     using System;
     using System.Collections.Generic;
@@ -18,7 +18,7 @@
             }
         }
 
-        public bool IsPersistent => false;
+        public bool IsPersistent => true;
 
         private MessageBrokerImpl()
         {
@@ -29,14 +29,14 @@
         {
             if (message == null || source == null)
                 return;
-            if (!_subscribers.ContainsKey(typeof(T)))
+            if(!_subscribers.ContainsKey(typeof(T)))
             {
                 return;
             }
             var delegates = _subscribers[typeof(T)];
             if (delegates == null || delegates.Count == 0) return;
             var payload = new MessagePayload<T>(message, source);
-            foreach (var handler in delegates.Select(item => item as Action<MessagePayload<T>>))
+            foreach(var handler in delegates.Select(item => item as Action<MessagePayload<T>>))
             {
                 Task.Factory.StartNew(() => handler?.Invoke(payload));
             }
@@ -45,7 +45,7 @@
         public void Subscribe<T>(Action<MessagePayload<T>> subscription)
         {
             var delegates = _subscribers.ContainsKey(typeof(T)) ? _subscribers[typeof(T)] : new List<Delegate>();
-            if (!delegates.Contains(subscription))
+            if(!delegates.Contains(subscription))
             {
                 delegates.Add(subscription);
             }
